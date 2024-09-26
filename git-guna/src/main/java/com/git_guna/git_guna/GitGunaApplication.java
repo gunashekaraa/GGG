baf5b3c033218aa90a8629b6a4d8d28a57eaea63
guna <gunashekarredddy88@gmail.com>
2024-09-26 19:44:08 +0530

    defect 3467 this links are not working in home page

---
+++ git-guna/src/main/java/com/git_guna/git_guna/GitGunaApplication.java
@@ -0,0 +1,13 @@
+package com.git_guna.git_guna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitGunaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitGunaApplication.class, args);
+	}
+
+}
---
+++ git-guna/src/test/java/com/git_guna/git_guna/GitGunaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.git_guna.git_guna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitGunaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
